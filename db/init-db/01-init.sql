b83595b544f07648bb6a3f6a9a32e59f14ad25f5
Ruslan Skv <skvirya@mail.ru>
2025-08-09 16:02:10 +0300

    все контейнеры запускаются, база данных загружается автоматически

---
+++ db/init-db/01-init.sql
@@ -0,0 +1,30 @@
+-- Создание таблицы film
+CREATE TABLE IF NOT EXISTS film (
+    id UUID PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    director VARCHAR(255) NOT NULL,
+    rating DECIMAL(3,1) NOT NULL,
+    tags JSONB,
+    about TEXT NOT NULL,
+    description TEXT NOT NULL,
+    image VARCHAR(255) NOT NULL,
+    cover VARCHAR(255) NOT NULL
+);
+
+-- Создание таблицы schedule
+CREATE TABLE IF NOT EXISTS schedule (
+    id UUID PRIMARY KEY,
+    daytime TIMESTAMP WITH TIME ZONE NOT NULL,
+    hall INTEGER NOT NULL,
+    rows INTEGER NOT NULL,
+    seats INTEGER NOT NULL,
+    price INTEGER NOT NULL,
+    taken JSONB,
+    "filmId" UUID NOT NULL,
+    CONSTRAINT fk_film
+    FOREIGN KEY("filmId") 
+    REFERENCES film(id)
+    ON DELETE CASCADE
+);
+
+
